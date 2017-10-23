98c8e5209f799df6a34910f184e7638e15e6a501
maury <maury.villafuerte@gmail.com>
2017-10-23 12:06:20 -0500

    panel

--- src/components/Panel.vue
+++ src/components/Panel.vue
@@ -3,19 +3,45 @@
     <aside class="aside--bar">
       <div class="box-itm">
         <h3>LOCATION</h3>
-        <input type="text" class="form-control">
+        <input type="text" class="form-control" placeholder="Enter a location e.g. Hamilton">
       </div>
       <div class="box-itm">
         <h3>PRICE RANGE</h3>
-        <input id="ex1" data-slider-id='ex1Slider' type="text" data-slider-min="0" data-slider-max="20" data-slider-step="1" data-slider-value="14"/>
+        <input id="ex2" type="text" class="span2 input--slider_1" value="" 
+          data-slider-min="10" 
+          data-slider-max="1000" 
+          data-slider-step="5" 
+          data-slider-value="[250,450]"/>
+        <span class="box--price">
+          <b>€ 10</b>  <b>€ 1000</b> 
+        </span>   
       </div>
       <div class="box-itm">
         <h3>BEDROOMS</h3>
-        <input type="text">
+        <input id="ex3" type="text" class="span2 input--slider_2" value="" 
+          data-slider-min="10" 
+          data-slider-max="1000" 
+          data-slider-step="5" 
+          data-slider-value="[250,450]"/>
+        <span class="box--price">
+          <b>€ 10</b>  <b>€ 1000</b> 
+        </span> 
       </div>
+      <!-- BOX FILTERS -->
+      <div class="box--filter">
+        <span>ADVANCED FILTERS</span>
+      </div>
+
       <div class="box-itm">
         <h3>BATHROOMS</h3>
-        <input type="text">
+        <input id="ex4" type="text" class="span2 input--slider_3" value="" 
+          data-slider-min="10" 
+          data-slider-max="1000" 
+          data-slider-step="5" 
+          data-slider-value="[250,450]"/>
+        <span class="box--price">
+          <b>€ 10</b>  <b>€ 1000</b> 
+        </span>
       </div>
       <div class="box-itm">
         <h3>LEASE DURATION</h3>
@@ -25,7 +51,7 @@
       </div>
       <div class="box-itm">
         <h3>LEASE DURATION</h3>
-        <input type="text" class="form-control">
+        <input type="text" class="form-control" placeholder="Enter Date">
       </div>
     </aside>
     <div class="map" id="google-maplima"></div>
@@ -41,8 +67,9 @@ export default {
     }
   },
   mounted (){
-
-
+    var slider = new Slider('.input--slider_1', {});
+    var slider = new Slider('.input--slider_2', {});
+    var slider = new Slider('.input--slider_3', {});
 
      function initMaplima() {
       var $map = $( '#google-maplima' );
@@ -74,10 +101,13 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  .box--filter{
+    background: #ccc;
+  }
   .aside--bar{
     width: 30%;
     height: 100vh;
-    background: orange;
+    background: #f1f1f1;
     padding: 20px;
     -webkit-box-sizing: border-box;
     -moz-box-sizing: border-box;
@@ -88,6 +118,9 @@ export default {
     width: 70%;
     height: 100vh;
   }
+  .box--price{
+    display: block;
+  }
   .box{
     display: -webkit-flex;
     display: -moz-flex;
@@ -95,5 +128,12 @@ export default {
     display: -o-flex;
     display: flex;
     justify-content: center;
+  }
+  .box-itm{
+    padding: 30px 0;
+  }
+  .box-itm h3{
+    font-size: 16px;
+    font-weight: bold;
   }
 </style>
