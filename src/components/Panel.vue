a2ca827ca0c216b42e2473276bb8b269cc29fa63
maury <maury.villafuerte@gmail.com>
2017-10-23 19:42:52 -0500

    map and login

--- src/components/Panel.vue
+++ src/components/Panel.vue
@@ -199,29 +199,39 @@ export default {
 
 
     var slider = new Slider('.input--slider_1', {});
-     function initMaplima() {
-      var $map = $( '#google-maplima' );
-      if ( ! $map.length ) return;
-      var myLatlng = new google.maps.LatLng(-12.086676, -77.058986,18); // <- Your latitude and longitude
-      
-      var mapOptions = {
-        zoom             : 15,
-        center           : myLatlng,
-        mapTypeControl   : true,
-        disableDefaultUI : true,
-        zoomControl      : true,
-        scrollwheel      : false,
-        draggable        : true
-      }
-      var map = new google.maps.Map(document.getElementById('google-maplima'), mapOptions);
 
-      var marker = new google.maps.Marker({
-          position: myLatlng,
-          map: map,
-          icon:'/static/location.png'
+    var locations = [
+      ['Bondi Beach', -33.890542, 151.274856, 4],
+      ['Coogee Beach', -33.923036, 151.259052, 5],
+      ['Cronulla Beach', -34.028249, 151.157507, 3],
+      ['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
+      ['Maroubra Beach', -33.950198, 151.259302, 1]
+    ];
+
+    var map = new google.maps.Map(document.getElementById('google-maplima'), {
+      zoom: 10,
+      center: new google.maps.LatLng(-33.92, 151.25),
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+
+    var infowindow = new google.maps.InfoWindow();
+
+    var marker, i;
+
+    for (i = 0; i < locations.length; i++) {  
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+        map: map,
+        icon:'/static/location.png'
       });
+
+      google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+          infowindow.setContent(locations[i][0]);
+          infowindow.open(map, marker);
+        }
+      })(marker, i));
     }
-    google.maps.event.addDomListener(window, 'load', initMaplima);   
   }
 
 }
